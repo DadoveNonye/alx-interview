6610a61a86fb977f72e723b3fb9e3ac9ea4209ea
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-05-03 04:18:57 +0100

    Add initial implementation of canUnlockAll function in 0x01-lockboxes/0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+def canUnlockAll(boxes):
+  """
+  This function determines if all boxes can be opened given a list of lists representing keys in each box.
+
+  Args:
+      boxes: A list of lists, where each sub-list represents the keys found in a box.
+
+  Returns:
+      True if all boxes can be opened, False otherwise.
+  """
+  visited = set() 
+  opened = [True] * len(boxes) 
+
+  def dfs(box_index):
+    """
+    Recursive helper function to explore reachable boxes using keys.
+
+    Args:
+        box_index: The index of the current box being explored.
+    """
+    if box_index in visited:
+      return
+
+    visited.add(box_index)
+    for key in boxes[box_index]:
+      if not opened[key]:
+        opened[key] = True
+        dfs(key)
+
+  dfs(0)  
+  return all(opened)  
